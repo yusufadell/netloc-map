268ec125a08ee34e435131f3759a9808a0f0ab63
yusufadell <youssef.31057404@ics.tanta.edu.eg>
2022-06-01 22:44:04 +0200

    💩 Extract network location from urls

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from browser_history import get_history
+from urllib.parse import urlparse
 
 
 def url_from_history():
@@ -24,3 +25,27 @@ def url_from_history():
     return urls
 
 
+def get_netloc(urls: list) -> list:
+    """get_netloc _summary_
+
+    Get the netloc from a list of urls
+
+    Args:
+        urls (list): A list of urls
+
+    Returns:
+        list: A list of netlocs
+
+    Examples:
+        >>> get_netloc(['https://www.google.com/search?channel=fs&client=ubuntu&q=github'', 'http://yahoo.com'])
+        ['google.com', 'yahoo.com']
+    """
+    netlocs = set()
+    for url in urls:
+        try:
+            # https://stackoverflow.com/questions/44113335/extract-domain-from-url-in-python
+            netloc = urlparse(url).netloc
+            netlocs.add(netloc)
+        except Exception as e:
+            print(e)
+    return netlocs
