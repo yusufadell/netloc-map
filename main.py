ce5a7beab8244efe2542b12810e9dcd9c2be90b6
Restyled.io <commits@restyled.io>
2022-06-16 23:42:59 +0000

    Restyled by black

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from .const import *
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import mpl_toolkits
+
 mpl_toolkits.__path__.append(MPL_PATH)
 
 
@@ -90,8 +91,9 @@ def get_ip(netlocs: list) -> list:
 
 
 async def do_req():
-    handler = ipinfo.AsyncHandler(ACCESS_TOKEN, cache_options={
-        'ttl': 30, 'maxsize': 128})
+    handler = ipinfo.AsyncHandler(
+        ACCESS_TOKEN, cache_options={"ttl": 30, "maxsize": 128}
+    )
     details = handler.getDetails(IP_ADDRESS)
     cd = list()
 
@@ -99,6 +101,7 @@ async def do_req():
     cd.append(details.all)
 
     return cd
+
 
 loop = asyncio.get_event_loop()
 complete_details = loop.run_until_complete(do_req())
@@ -109,8 +112,8 @@ lat = []
 lon = []
 
 for loc in complete_details:
-    lat.append(float(loc.result()['latitude']))
-    lon.append(float(loc.result()['longitude']))
+    lat.append(float(loc.result()["latitude"]))
+    lon.append(float(loc.result()["longitude"]))
 
 
 # Basic map plot
@@ -118,39 +121,61 @@ fig, ax = plt.subplots(figsize=(40, 20))
 map = Basemap()
 
 # dark grey land, black lakes
-map.fillcontinents(color='#2d2d2d', lake_color='#000000')
+map.fillcontinents(color="#2d2d2d", lake_color="#000000")
 
 # black background
-map.drawmapboundary(fill_color='#000000')
+map.drawmapboundary(fill_color="#000000")
 
 # thin white line for country borders
 map.drawcountries(linewidth=0.15, color="w")
 
 map.drawstates(linewidth=0.1, color="w")
 
-map.plot(lon, lat, linestyle='none', marker="o",
-         markersize=25, alpha=0.4, c="white", markeredgecolor="silver",
-         markeredgewidth=1)
+map.plot(
+    lon,
+    lat,
+    linestyle="none",
+    marker="o",
+    markersize=25,
+    alpha=0.4,
+    c="white",
+    markeredgecolor="silver",
+    markeredgewidth=1,
+)
 
 
 def update_plot(frame_number):
     # update the map
-    map.plot(lon[frame_number], lat[frame_number], linestyle='none', marker="o",
-             markersize=25, alpha=0.4, c="white", markeredgecolor="silver",
-             markeredgewidth=1)
+    map.plot(
+        lon[frame_number],
+        lat[frame_number],
+        linestyle="none",
+        marker="o",
+        markersize=25,
+        alpha=0.4,
+        c="white",
+        markeredgecolor="silver",
+        markeredgewidth=1,
+    )
 
 
 def init():
-    plt.text(-170, -72, 'Server locations of top 500 websites '
-             '(by traffic)\nPlot realized with Python and the Basemap library'
-             '\n\n~Yusuf\n yusufadell.dev@gmail.com', ha='left', va='bottom',
-             size=28, color='silver')
+    plt.text(
+        -170,
+        -72,
+        "Server locations of top 500 websites "
+        "(by traffic)\nPlot realized with Python and the Basemap library"
+        "\n\n~Yusuf\n yusufadell.dev@gmail.com",
+        ha="left",
+        va="bottom",
+        size=28,
+        color="silver",
+    )
 
 
-ani = animation.FuncAnimation(fig, update_plot, interval=1,
-                              frames=490, init_func=init)
+ani = animation.FuncAnimation(fig, update_plot, interval=1, frames=490, init_func=init)
 
 
-writer = animation.writers['ffmpeg']
-writer = writer(fps=20, metadata=dict(artist='Yusufadel'), bitrate=1800)
-ani.save('anim.mp4', writer=writer)
+writer = animation.writers["ffmpeg"]
+writer = writer(fps=20, metadata=dict(artist="Yusufadel"), bitrate=1800)
+ani.save("anim.mp4", writer=writer)
